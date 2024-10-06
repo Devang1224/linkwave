f38a71209daee2a554105d783d7023b5950ce561
Devang1224 <97470914+Devang1224@users.noreply.github.com>
2024-10-07 00:42:21 +0530

    ui: updated homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,25 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Roboto_Mono } from 'next/font/google'
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const robotoMono = localFont({
+  src: "./fonts/RobotoMono-VariableFont_wght.ttf",
+  variable: "--font-roboto-mono",
+  weight: "100 200 300 400 500 600 700 800 900",
 });
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+
+const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto-mono',
+})
+ 
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${roboto_mono.variable} ${geistMono.variable} antialiased`}
       >
           {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
 import Image from "next/image";
+import Link from "next/link";
 
 
 export default function Home() {
   return (
-     <div>
+     <div className="home_background">
+              <nav className="flex items-center justify-between p-4 px-10">
+                <p className="text-xl">LinkWave</p>
+                <div>
+                  <Link href="/log-in" className="p-2 px-4 border rounded-md border-white/50">
+                    Log In
+                  </Link>
+                </div>
+              </nav>
 
+              <div className="p-10">
+                <p 
+                className="text-4xl font-medium text-center mt-10 tracking-widest [mask-image:linear-gradient(to_right,transparent,black,transparent)]"
+
+                >LISTEN WHAT THE WORLD IS LISTENING</p>
+              </div>
      </div>
   );
 }
